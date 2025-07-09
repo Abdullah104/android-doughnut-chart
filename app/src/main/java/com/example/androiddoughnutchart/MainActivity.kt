18bea20664cec09dca5b3c9ea7bf1492546039df
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-07-09 08:59:58 +0300

    draw triangle

--- app/src/main/java/com/example/androiddoughnutchart/MainActivity.kt
+++ app/src/main/java/com/example/androiddoughnutchart/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddoughnutchart.ui.theme.AndroidDoughnutChartTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidDoughnutChartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Greeting()
+                    }
                 }
             }
         }
@@ -31,17 +38,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(8.dp)
+    ) {
+        val path = Path().apply {
+            moveTo(center.x, 0f)
+            lineTo(size.width, size.height)
+            lineTo(0f, size.height)
+            lineTo(center.x, 0f)
+            close()
+        }
+
+        drawPath(path = path, color = Color.Red, style = Stroke())
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidDoughnutChartTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
